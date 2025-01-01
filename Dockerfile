a0d56b771a27106491d84e87a9edbd2f9b8200b9
paulheu <paulvdheu@hotmail.com>
2025-01-01 10:59:59 +0000

    main update
    updated to latest known working config

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,13 @@
 FROM debian:latest
-MAINTAINER Adam Talsma <se-adam.talsma@ccpgames.com>
+LABEL MAINTAINER="Paul van der Heu<pvdh@outlook.com>"
 
 ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt-get update -qty
-RUN apt search mysql-server
-RUN apt-get install -qqy php8.2 php8.2-cli php8.2-mcrypt php8.2-intl php8.2-mysql php8.2-curl php8.2-gd curl git default-mysql-client default-mysql-server expect
+RUN apt-get -qq update
+RUN apt-get -qq install php php-cli php-mcrypt php-intl php-mysql php-curl php-gd php-mbstring curl git mysql-client mysql-server expect
 
 RUN git clone https://bitbucket.org/daimian/tripwire.git /var/www/tripwire
-RUN curl -L https://bitbucket.org/daimian/tripwire/downloads/tripwire.sql > /tmp/tripwire.sql
-RUN curl -L https://bitbucket.org/daimian/tripwire/downloads/eve_api.sql > /tmp/eve_api.sql
+RUN curl -L https://bitbucket.org/daimian/tripwire/downloads/tripwire.sql > /var/www/tripwire/.docker/mysql/tripwire.sql
 
 RUN chown -R www-data:www-data /var/www/tripwire
 
@@ -18,4 +16,4 @@ COPY entrypoint.sh /entrypoint.sh
 EXPOSE 80
 WORKDIR /var/www/tripwire
 
-CMD /entrypoint.sh
+CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -18,9 +18,9 @@ DB_USERNAME=${DB_USERNAME-tripwire}
 DB_PASSWORD=${DB_PASSWORD-secret}
 ADMIN_EMAIL=${ADMIN_EMAIL-"webmaster@localhost"}
 SERVER_NAME=${SERVER_NAME-"tripwire.local"}
-ADMIN_PASSWORD=${ADMIN_PASSWORD-admin}
 
-MYSQL_ROOT_PASS=$(echo -e `date` | md5sum | awk '{ print $1 }');
+DBRPWD=(echo -e `date` | md5sum | awk '{ print $1 }');
+DB_ROOTPASS=${DB_ROOTPASS-$DBRPWD}
 
 service mysql restart
 
@@ -34,9 +34,9 @@ send \"\r\"
 expect \"Set root password?\"
 send \"y\r\"
 expect \"New password:\"
-send \"$MYSQL_ROOT_PASS\r\"
+send \"$DB_ROOTPASS\r\"
 expect \"Re-enter new password:\"
-send \"$MYSQL_ROOT_PASS\r\"
+send \"$DB_ROOTPASS\r\"
 expect \"Remove anonymous users?\"
 send \"y\r\"
 expect \"Disallow root login remotely?\"
@@ -51,39 +51,41 @@ expect eof
 apt-get remove -y expect
 apt-get autoremove -y
 
-mysql -uroot -p$MYSQL_ROOT_PASS -e "create database tripwire;"
-mysql -uroot -p$MYSQL_ROOT_PASS -e "create database eve_api;"
-mysql -uroot -p$MYSQL_ROOT_PASS -e "GRANT ALL ON tripwire.* to '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -uroot -p$MYSQL_ROOT_PASS -e "GRANT ALL ON eve_api.* to '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -uroot -p$MYSQL_ROOT_PASS -e "GRANT SUPER ON *.* to '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -uroot -p$DB_ROOTPASS -e "create database tripwire;"
+mysql -uroot -p$DB_ROOTPASS -e "GRANT ALL ON tripwire.* to '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -uroot -p$DB_ROOTPASS -e "GRANT SUPER ON *.* to '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
 
-mysql -u$DB_USERNAME -p$DB_PASSWORD tripwire < /tmp/tripwire.sql
-mysql -u$DB_USERNAME -p$DB_PASSWORD eve_api < /tmp/eve_api.sql
-mysql -u$DB_USERNAME -p$DB_PASSWORD -e "INSERT INTO eve_api.cacheTime (type, time) VALUES ('activity', now())"
+mysql -u$DB_USERNAME -p$DB_PASSWORD tripwire < /var/www/tripwire/.docker/mysql/tripwire.sql
 
-mv db.inc.example.php db.inc.php
-sed -i -e "s/host=localhost/host=$DB_HOSTNAME/" db.inc.php
-sed -i -e "s/dbname=tripwire_database/dbname=tripwire/" db.inc.php
-sed -i -e "s/username/$DB_USERNAME/" db.inc.php
-sed -i -e "s/password/$DB_PASSWORD/" db.inc.php
-sed -i -e "s/clientID/$SSO_CLIENT_ID/" db.inc.php
-sed -i -e "s/secret/$SSO_SECRET_KEY/" db.inc.php
+
+cp /var/www/tripwire/db.inc.example.php /var/www/tripwire/db.inc.php
+sed -i -e "s/host=localhost/host=$SERVER_NAME/g" /var/www/tripwire/db.inc.php
+sed -i -e "s/dbname=tripwire_database/dbname=$DBNAME/g" /var/www/tripwire/db.inc.php
+sed -i -e "s/username/$DB_USERNAME/g" /var/www/tripwire/db.inc.php
+sed -i -e "s/password/$DB_USERPWD/g" /var/www/tripwire/db.inc.php
+
+cp /var/www/tripwire/config.example.php /var/www/tripwire/config.php
+sed -i -e "s/false)/True)/g" /var/www/tripwire/config.php
+sed -i -e "s/localhost/$SERVER_NAME/g" /var/www/tripwire/config.php
+sed -i -e "s/adminEmail@example.com/$ADMIN_EMAIL/g" /var/www/tripwire/config.php
+sed -i -e "s/clientID/$SSO_CLIENT_ID/g" /var/www/tripwire/config.php
+sed -i -e "s/secret/$SSO_SECRET_KEY/g" /var/www/tripwire/config.php
+sed -i -e "s/http://localhost/index.php?mode=sso/$SSO_REDIRECT/g" /var/www/tripwire/config.php
 
 echo "ServerName $SERVER_NAME" >> /etc/apache2/apache2.conf
 unlink /etc/apache2/sites-enabled/000-default.conf
-sed -i -e "s/Options Indexes FollowSymLinks/Options FollowSymLinks/" /etc/apache2/apache2.conf
-sed -i -e "s/ServerTokens OS/ServerTokens Prod/" /etc/apache2/conf-enabled/security.conf
-sed -i -e "s/ServerSignature On/ServerSignature Off/" /etc/apache2/conf-enabled/security.conf
+sed -i -e "s/ServerTokens OS/ServerTokens Prod/g" /etc/apache2/conf-enabled/security.conf
+sed -i -e "s/ServerSignature On/ServerSignature Off/g" /etc/apache2/conf-enabled/security.conf
 
 cat <<EOF >> /etc/apache2/sites-available/100-$SERVER_NAME.conf
 <VirtualHost *:80>
     ServerAdmin $ADMIN_EMAIL
-    DocumentRoot "/var/www/tripwire"
+    DocumentRoot "/var/www/tripwire/public"
     ServerName $SERVER_NAME
     ServerAlias www.$SERVER_NAME
     ErrorLog /var/log/apache2/$SERVER_NAME-error.log
     CustomLog /var/log/apache2/$SERVER_NAME-access.log combined
-    <Directory "/var/www/tripwire">
+    <Directory "/var/www/tripwire/public">
         AllowOverride All
         Order allow,deny
         Allow from all
@@ -94,37 +96,3 @@ EOF
 ln -s /etc/apache2/sites-available/100-$SERVER_NAME.conf /etc/apache2/sites-enabled/
 
 service apache2 restart
-apachectl -t -D DUMP_VHOSTS
-php --version
-
-cat <<EOF > /tmp/api_pull.patch
-*** tools/api_pull.php.orig	Fri Jan 22 18:01:57 2016
---- tools/api_pull.php	Fri Jan 22 19:19:45 2016
-***************
-*** 1,14 ****
-  <?php
-
-  session_start();
-
-- if(!isset($_SESSION['super']) || $_SESSION['super'] != 1) {
-- 	echo 'Security Failure!';
-- 	exit();
-- }
--
-  ini_set('display_errors', 'On');
-
-  require('../db.inc.php');
-  require('../api.class.php');
-
---- 1,9 ----
-EOF
-
-patch /var/www/tripwire/tools/api_pull.php /tmp/api_pull.patch
-cd /var/www/tripwire/tools/
-
-while [[ 1 ]]; do
-    /usr/bin/php api_pull.php > /dev/null 2>&1
-    for I in {1..180}; do
-      sleep 1
-    done
-done
